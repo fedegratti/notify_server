4511f9a0b9ac0f47dd459490211c74df31ac113c
Federico Gratti <federicogratti@gmail.com>
2026-01-18 11:44:15 -0300

    Implement Notifications::Channel::Email object service

--- app/services/notifications/channels/email.rb
+++ app/services/notifications/channels/email.rb
@@ -4,8 +4,32 @@ module Notifications
   module Channels
     class Email < ApplicationService
       def call(notification)
-        # Logic to send email notification
-        puts "Sending email notification: #{notification.content}"
+        send_email(notification)
+      end
+
+      private
+
+      def send_email(notification)
+        conn = Faraday.new(
+          url: ENV.fetch('EMAIL_API_BASE_URL', nil),
+          headers: { 'Content-Type' => 'application/json' }
+        )
+
+        response = conn.post('/send') do |req|
+          req.body = notification_params(notification).to_json
+        end
+
+        puts "Email sent! Response status: #{response.status}"
+      end
+
+      def notification_params(notification)
+        {
+          notification: {
+            title: notification.title,
+            content: notification.content,
+            email: notification.user.email
+          }
+        }
       end
     end
   end
